9fe706e4359c508f867c33f26bcaf9f4b68632db
wanglei <wanglei@wdai.com>
2018-03-09 16:16:31 +0800

    test10

--- ripple-webapp/src/main/java/com/stone/ripple/util/TestUtil.java
+++ ripple-webapp/src/main/java/com/stone/ripple/util/TestUtil.java
@@ -2,4 +2,14 @@ package com.stone.ripple.util;
 
 public class TestUtil {
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
